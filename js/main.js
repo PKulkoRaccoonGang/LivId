0a3377e07c10e92a3af4dc46c48719f3afd95eae
sergiy kostiv <sergiy,kostiv@gmail.com>
2023-04-09 20:21:50 +0300

    fix : fixed buttons-block

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 const burger = document.querySelector('.menu__burger');
 const menu = document.querySelector('.menu__list');
 const body = document.querySelector('body');
+const btn = document.querySelector('.header__buttons');
+const head = document.querySelector('.header__wrapper');
 
 burger.addEventListener('click', () => {
   menu.classList.toggle('menu__list-visible');
   burger.classList.toggle('menu__burger-active');
   body.classList.toggle('active');
+  btn.classList.toggle('header__buttons-active');
+  head.classList.toggle('header__wrapper-active');
 });
