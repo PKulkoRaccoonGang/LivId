3d2122a485bf8d6e7c9db5fdc5e9db72f6f10a07
Sergiy Kostiv <sergiy.kostiv@gmail.com>
2023-04-23 20:52:33 +0300

    fix: fixed menu burger

--- js/main.js
+++ js/main.js
@@ -1,13 +1,9 @@
 const burger = document.querySelector('.menu__burger');
 const menu = document.querySelector('.menu__list');
-const body = document.querySelector('body');
-const btn = document.querySelector('.header__buttons');
-const head = document.querySelector('.header__wrapper');
+const buttons = document.querySelector('.header__buttons');
 
 burger.addEventListener('click', () => {
   menu.classList.toggle('menu__list-visible');
   burger.classList.toggle('menu__burger-active');
-  body.classList.toggle('active');
-  btn.classList.toggle('header__buttons-active');
-  head.classList.toggle('header__wrapper-active');
+  buttons.classList.toggle('header__buttons-visible');
 });
